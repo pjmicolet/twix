0876988934c30d694358d722c699aa5490522f13
pjm <p.micolet@gmail.com>
2024-05-25 10:52:33 +0100

    Adding rom again

---
+++ src/components/rom.cpp
@@ -0,0 +1,52 @@
+#include "rom.hpp"
+#include <fstream>
+
+namespace Components {
+	template<typename Storage>
+	Rom<Storage>::Rom(const std::filesystem::path &rom_path) {
+	  if (std::filesystem::exists(rom_path)) {
+		if (!load_rom(rom_path)) {
+		  throw RomException("Found rom but couldn't load");
+		}
+	  } else {
+		throw RomException("Invalid rom path");
+	  }
+	}
+
+	template<typename Storage>
+	auto Rom<Storage>::load_rom(const std::filesystem::path &path) -> bool {
+
+	  std::ifstream rom(path, std::ios::binary | std::ios::ate);
+	  // Maybe should throw but idk
+	  if (!rom)
+		return false;
+
+	  auto end = rom.tellg();
+	  rom.seekg(0, std::ios::beg);
+
+	  auto size = std::size_t(end - rom.tellg());
+	  if (size == 0)
+		return false;
+
+	  data_.resize(size);
+	  if (!rom.read((char *)data_.data(), static_cast<std::streamsize>(data_.size())))
+		return false;
+
+	  return true;
+	}
+
+	template<typename Storage>
+	auto Rom<Storage>::begin() -> typename std::vector<Storage>::iterator {
+	  return data_.begin();
+	}
+
+	template<typename Storage>
+	auto Rom<Storage>::end() -> typename std::vector<Storage>::iterator {
+	  return data_.end();
+	}
+
+	template<typename Storage>
+	auto Rom<Storage>::at(size_t index) -> Storage {
+	  return data_[index];
+	}
+};
---
+++ src/components/rom.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstddef>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace Components {
+	class RomException : public std::exception {
+	public:
+	  RomException(std::string message) : exception_msg_(message){};
+	  virtual const char *what() const throw() { return exception_msg_.c_str(); }
+
+	private:
+	  std::string exception_msg_;
+	};
+
+	template<typename Storage>
+	class Rom {
+	public:
+	  // For testing only
+	  Rom(const std::filesystem::path &rom_path);
+
+	  Rom(){};
+
+	  // Could be in utils idk
+	  auto load_rom(const std::filesystem::path &path) -> bool;
+	  auto size() -> size_t { return data_.size(); };
+	  auto begin() -> typename std::vector<Storage>::iterator;
+	  auto end() -> typename std::vector<Storage>::iterator;
+	  auto at(size_t index) -> Storage;
+
+	private:
+	  std::vector<Storage> data_;
+	};
+};
