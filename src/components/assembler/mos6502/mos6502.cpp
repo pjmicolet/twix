bd117987e4f70f378161631038e83f5774702ad9
pjm <p.micolet@gmail.com>
2024-07-07 18:55:37 +0100

    some more stuff

--- src/components/assembler/mos6502/mos6502.cpp
+++ src/components/assembler/mos6502/mos6502.cpp
@@ -12,4 +12,13 @@ namespace assembler { namespace mos6502 {
 	auto mos6502Assembler::assemble(const std::string& inst, std::vector<byte_type>& result) -> void {
 	}
 
+	auto Inst::match(std::string operand) -> byte_type {
+		std::string_view opView{operand};
+		for(auto& possibleTypes : types) {
+			if(possibleTypes.match(opView)) {
+				return possibleTypes.getType();
+			}
+		}
+		throw std::runtime_error("Made up instruction");	
+	}
 };};
--- src/components/assembler/mos6502/mos6502.hpp
+++ src/components/assembler/mos6502/mos6502.hpp
@@ -2,16 +2,25 @@
 #include <string>
 #include <string_view>
 #include <cstdint>
+#include <unordered_map>
 
 namespace assembler {
     namespace mos6502 {
+		using byte_type = char;
+
         struct OpType {
-            constexpr OpType(const uint8_t opType, const uint8_t opCode, std::string_view& opPattern) : 
-                code_(opCode), opPattern_(opPattern){}
+            constexpr OpType(const uint8_t opType, const uint8_t opCode, const std::string_view& opPattern) : 
+                type_(opType), code_(opCode), opPattern_(opPattern){}
+			auto getType() -> char {
+				return code_;
+			}
+			auto match(std::string_view& v) -> bool {
+				return utils::match(v,opPattern_);
+			}
             private:
                 uint8_t type_;
                 uint8_t code_;
-                std::string_view& opPattern_;
+                const std::string_view& opPattern_;
         };
 
         struct OpTypeEncoding {
@@ -30,26 +39,37 @@ namespace assembler {
             static inline constexpr std::string_view indirectY = "($@byte),Y";
         };
 
-        #define IMPL Operation(0, opCode, OpTypeEncoding::implicit);
-        #define ACC Operation(1, opCode, OpTypeEncoding::accumulator);
-        #define IMM Operation(2, opCode, OpTypeEncoding::immediate);
-        #define ZP Operation(3, opCode, OpTypeEncoding::zeroPage);
-        #define ZPX Operation(4, opCode, OpTypeEncoding::zeropPageX);
-        #define ZPY Operation(5, opCode, OpTypeEncoding::zeroPageY);
-        #define IZX Operation(6, opCode, OpTypeEncoding::indirectX);
-        #define IZY Operation(7, opCode, OpTypeEncoding::indirectY);
-        #define ABS Operation(8, opCode, OpTypeEncoding::absolute);
-        #define ABX Operation(9, opCode, OpTypeEncoding::absoluteX);
-        #define ABY Operation(10, opCode, OpTypeEncoding::absoluteY);
-        #define IND Operation(11, opCode, OpTypeEncoding::indirect);
-        #define REL Operation(12, opCode, OpTypeEncoding::relative);
+        #define IMPL(opCode) OpType{0, opCode, OpTypeEncoding::implicit}
+        #define ACC(opCode) OpType{1, opCode, OpTypeEncoding::accumulator}
+        #define IMM(opCode) OpType{2, opCode, OpTypeEncoding::immediate}
+        #define ZP(opCode) OpType{3, opCode, OpTypeEncoding::zeroPage}
+        #define ZPX(opCode) OpType{4, opCode, OpTypeEncoding::zeropPageX}
+        #define ZPY(opCode) OpType{5, opCode, OpTypeEncoding::zeroPageY}
+        #define IZX(opCode) OpType{6, opCode, OpTypeEncoding::indirectX}
+        #define IZY(opCode) OpType{7, opCode, OpTypeEncoding::indirectY}
+        #define ABS(opCode) OpType{8, opCode, OpTypeEncoding::absolute}
+        #define ABX(opCode) OpType{9, opCode, OpTypeEncoding::absoluteX}
+        #define ABY(opCode) OpType{10, opCode, OpTypeEncoding::absoluteY}
+        #define IND(opCode) OpType{11, opCode, OpTypeEncoding::indirect}
+        #define REL(opCode) OpType{12, opCode, OpTypeEncoding::relative}
 
+		struct Inst {
+			Inst(std::string name, std::vector<OpType> types) {}
+			auto match(std::string operand) -> byte_type;
+			private:
+				std::string name_;
+				std::vector<OpType> types;
+		};
+
+		#define INSTRUCTION(name, ...) { name, Inst{name, {__VA_ARGS__}} }
 
         struct mos6502Assembler {
-			using byte_type = char;
 			auto assemble(const std::vector<std::string>& source) -> std::vector<byte_type>;
 			private:
 				auto assemble(const std::string& inst, std::vector<byte_type>& result) -> void;
+				std::unordered_map<std::string, Inst> insts = {
+					INSTRUCTION("ADD", IMPL(0), ACC(2), IMM(3))	
+				};
         };
 
         struct mos6502Disassembler {
