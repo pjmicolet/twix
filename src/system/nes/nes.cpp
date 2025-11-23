ce7843b179e2ddd2f529a4e375103bc672fbfdef
pjmicolet <p.micolet@gmail.com>
2025-11-23 21:06:29 +0000

    gt rid of print cuz devin sucks

--- src/system/nes/nes.cpp
+++ src/system/nes/nes.cpp
@@ -1,10 +1,8 @@
 #include <cores/mos6502/cpu.hpp>
-#include <print>
 #include "nes.hpp"
 
 int main(int argc, char** argv) {
   std::string rom_path = argv[1];
-  std::print("Hey this is {}", rom_path);
   cores::mos6502::NesRom rom{rom_path};
   NesRAM<Mapper0> ram{rom};
   cores::mos6502::mos6502<NesRAM<Mapper0>> core{ram};
